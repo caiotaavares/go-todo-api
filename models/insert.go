30c9cc5e1b5b5ab7faf3223c8e05b128128a17d1
BlueLedbetter <caio_s.tavares2@hotmail.com>
2022-10-11 17:09:36 -0300

    Insert query

--- models/insert.go
+++ models/insert.go
@@ -11,5 +11,9 @@ func Insert(todo Todo) (id int64, err error) {
 	}
 	defer conn.Close()
 
+	sql := `INSERT INTO todos(titile, description, done) VALUES ($1, $2, $3) RETURNING id`
+
+	err = conn.QueryRow(sql, todo.Title, todo.Description, todo.Done).Scan(&id)
+
 	return
 }
