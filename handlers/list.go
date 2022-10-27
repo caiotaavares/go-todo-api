1d129cf8a6a4e7372bb98a44d86057c5c88332cc
BlueLedbetter <caio_s.tavares2@hotmail.com>
2022-10-27 00:04:45 -0300

    fix database connection

--- handlers/list.go
+++ handlers/list.go
@@ -4,21 +4,13 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/353solutions/nlp/models"
-	"github.com/go-chi/chi/v5"
 )
 
 func List(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(chi.URLParam(r, "id"))
-	if err != nil {
-		log.Printf("Erro ao fazer parse do id: %v", err)
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
 
-	todo, err := models.Get(int64(id))
+	todo, err := models.GetAll()
 	if err != nil {
 		log.Printf("Erro ao atualizar registro: %v", err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
--- main.go
+++ main.go
@@ -20,6 +20,6 @@ func main() {
 	r.Put("/{id}", handlers.Update)
 	r.Delete("/{id}", handlers.Delete)
 	r.Get("/", handlers.List)
-	// r.Get("/{id}", handlers.Get)
+	r.Get("/{id}", handlers.Get)
 	http.ListenAndServe(fmt.Sprintf(":%s", configs.GetServerPort()), r)
 }
